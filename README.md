1e82b73510dc3b6b74809d9106c839e7324fb13b
Gabriel Russo <gabrielrusso@protonmail.com>
2025-08-12 23:27:25 -0400

    Unique pointer; Exibir metadados; README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# sensoriamento-remoto-c-c-
+# Sensoriamento Remoto com C/C++
+
+https://gdal.org/en/stable/tutorials/raster_api_tut.html
+
+https://gdal.org/en/stable/user/raster_data_model.html#raster-data-model
--- src/rgb.cpp
+++ src/rgb.cpp
@@ -1,6 +1,10 @@
+// Copyright (C) 2025 Gabriel Russo
+// See end of file for extended copyright information.
+
 #include <iostream>
-#include <gdal/gdal_priv.h>
+#include <memory>
 #include <errno.h>
+#include <gdal/gdal_priv.h>
 
 int main(int argc, const char *argv[])
 {
@@ -22,7 +26,7 @@ int main(int argc, const char *argv[])
   const char *banda_b = argv[3];
   std::cout << "Banda azul: " << banda_b << std::endl;
 
-  GDALDataset *r_ds = GDALDataset::FromHandle(GDALOpen(banda_r, GA_ReadOnly));
+  std::unique_ptr<GDALDataset> r_ds(GDALDataset::FromHandle(GDALOpen(banda_r, GA_ReadOnly)));
 
   if (!r_ds)
   {
@@ -30,9 +34,41 @@ int main(int argc, const char *argv[])
     return 1;
   }
 
-  std::cout << "Gdal DS: " << r_ds->GetDriver()->GetDescription() << std::endl;
+  printf(
+      "---> Informações do Driver:\n Nome curto: %s | Nome longo: %s\n",
+      r_ds->GetDriver()->GetDescription(),
+      r_ds->GetDriver()->GetMetadataItem(GDAL_DMD_LONGNAME));
 
-  r_ds->Close();
+  printf(
+      "---> Estrutura:\n Linhas: %d | Colunas: %d | Bandas: %d\n",
+      r_ds->GetRasterXSize(), r_ds->GetRasterYSize(), r_ds->GetRasterCount());
+
+  // Chamada do método pra pegar o SRC pode retornar nulo, cuidado.
+  printf(
+      "---> SRC: %s\n",
+      r_ds->GetProjectionRef());
 
   return 0;
 }
+
+// MIT License
+//
+// Copyright (C) 2025 Gabriel Russo
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
